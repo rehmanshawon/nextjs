85273b9df7ded6c26027a60e61877e07e55d4590
rehmanshawon <meghnadbadh@gmail.com>
2023-07-31 18:51:37 +0600

    aa

--- next.config.js
+++ next.config.js
@@ -40,8 +40,8 @@ const nextConfig = {
   publicRuntimeConfig: {
     apiUrl:
       process.env.NODE_ENV === "development"
-        ? "http://142.93.208.249:3000/api" // development api
-        : "http://142.93.208.249:3000/api", // production api
+        ? "http://localhost:3000/api" // development api
+        : "http://localhost:3000/api", // production api
   },
 };
 
